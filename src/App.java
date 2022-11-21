a8c479617804f851ef3089d5ed235f5da0125a21
Sallai András <joska@zold.and>
2022-11-21 10:27:15 +0100

    Fájlkezelés és számítás kész

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+/*
+ * File: App.java
+ * Created Date: 2020-09-15
+ * Author: Nagy János
+ * Github: https://github.com/andteki
+ * Copyright (c) 2020-2022 Nagy János
+ * 
+ * GNU GPL v2
+ */
+
+import controllers.MainController;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new MainController();
     }
 }
+
+
+/*
+ * 2020-09-15 Projekt alapja kész.
+ * 2020-09-25 Átírva grafikus felületre.
+ * 2020-10-21 Névjegy rész kész.
+ */
